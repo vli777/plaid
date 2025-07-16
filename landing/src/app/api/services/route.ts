2a9e1be54a9abf96c5e950c0eb1f6bbd8056a044
vintg <10236284+vintg@users.noreply.github.com>
2025-07-16 01:08:59 -0400

    chore: lint

--- landing/src/app/api/services/route.ts
+++ landing/src/app/api/services/route.ts
@@ -1,6 +1,6 @@
-import { NextResponse } from 'next/server'
-import { services } from '../../../mocks/mockServicesData'
+import { NextResponse } from "next/server";
+import { services } from "../../../mocks/mockServicesData";
 
 export async function GET() {
-  return NextResponse.json(services)
+  return NextResponse.json(services);
 }
