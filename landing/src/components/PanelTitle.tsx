b7096edf6264d59ee3ebdd71fbed87ff28bfee3c
vintg <10236284+vintg@users.noreply.github.com>
2025-07-16 23:42:12 -0400

    refactor, style(PanelTitle): change positioning to absolute and adjust padding

--- landing/src/components/PanelTitle.tsx
+++ landing/src/components/PanelTitle.tsx
@@ -7,14 +7,19 @@ export interface PanelTitleProps {
 export default function PanelTitle({ panel }: PanelTitleProps) {
   return (
     <div
-      className="fixed inset-y-16 left-0 w-[360px] flex flex-col justify-between px-8 py-6
+      className="
+        absolute                 
+        inset-y-0 left-0         
+        w-[360px]
+        flex flex-col justify-between
+        px-8 py-6                
       "
     >
-      <h1 className="text-6xl md:text-8xl tracking-tight leading-[0.8] font-extrabold uppercase">
+      <h1 className="text-6xl md:text-8xl tracking-tight leading-[0.8] pt-16 font-extrabold uppercase">
         {panel.title}
       </h1>
 
-      <div className="text-[5rem] md:text-[7rem] font-bold leading-none tracking-tighter text-stone-200">
+      <div className="text-[5rem] md:text-[7rem] font-bold leading-none tracking-tighter">
         {panel.order < 10 ? `0${panel.order}` : panel.order}
       </div>
     </div>
